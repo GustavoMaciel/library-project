8d2e105dc07858420f03dccfc0c58f74bbff3c74
Gustavo Maciel <gustavo.nunes@virtus.ufcg.edu.br>
2020-09-25 12:31:07 -0300

    Lindando com Cors

--- library-front/src/app/components/author/author-list/author-list.component.ts
+++ library-front/src/app/components/author/author-list/author-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../../shared/services/crud.service';
 
 @Component({
   selector: 'app-author-list',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorListComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  items: any[] = [];
+  pageSize = 10;
+  currentPage = 0;
+  totalRecords = 0;
+  totalPages = 0;
+
+  constructor(private service: CrudService) { }
 
   ngOnInit() {
+    this.listItems();
+  }
+
+  listItems() {
+    this.loading = true;
+    this.service.get('http://localhost:8080/library/authors').subscribe((res: any) => {
+      this.items = res.items;
+      this.pageSize = res.pageSize;
+      this.currentPage = res.currentPage;
+      this.totalRecords = res.totalRecords;
+      this.totalPages = res.totalPages
+      this.loading = false;
+      console.log(res);
+    }, err => {
+      console.log(err);
+      this.loading = false;
+    })
   }
 
 }
