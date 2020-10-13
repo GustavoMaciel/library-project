930f64ed8c3cf86ce52d4e2748a69fd545407487
Gustavo Maciel <gustavo.nunes@virtus.ufcg.edu.br>
2020-10-13 11:24:38 -0300

    List methods with composition

--- library-front/src/app/components/book/book-list/book-list.component.ts
+++ library-front/src/app/components/book/book-list/book-list.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import { BookURL } from 'src/app/shared/url/url.domain';
 import { ModalService } from '../../../shared/services/modal.service';
+import { ListUtils } from '../../../shared/utils/list-utils';
 
 @Component({
   selector: 'app-book-list',
@@ -12,111 +13,23 @@ import { ModalService } from '../../../shared/services/modal.service';
 })
 export class BookListComponent implements OnInit {
 
-  books: any[] = [];
-  removeItem: any;
-  pageSize = 10;
-  currentPage = 0;
-  totalRecords = 0;
-  totalPages = 0;
-  currentSearch = '';
-  loading = false;
+  listUtils: ListUtils;
 
   constructor(
-    private crudService: CrudService,
-    private router: Router,
-    private notificationService: NotificationService,
-    private modalService: ModalService
-  ) { }
+      private crudService: CrudService,
+      private router: Router,
+      private notificationService: NotificationService,
+      private modalService: ModalService
+    ) {
+    this.listUtils = new ListUtils(crudService, router, notificationService, modalService, BookURL.BASE, BookURL.BASE);
+  }
 
   ngOnInit() {
-    this.listItems();
-  }
-
-  getServiceURL(): string {
-    return BookURL.BASE;
-  }
-
-  getRouterURL(): string {
-    return 'books';
-  }
-
-  postGetItems(): void {
-  }
-
-  listItems(): void {
-    this.loading = true;
-    this.crudService.getAll(this.getServiceURL(), this.generatePagination()).subscribe((res: any) => {
-      this.books = res.items;
-      this.pageSize = res.pageSize;
-      this.currentPage = res.currentPage;
-      this.totalRecords = res.totalRecords;
-      this.totalPages = res.totalPages;
-      this.loading = false;
-      this.postGetItems();
-    }, (err) => {
-      this.loading = false;
-    });
-  }
-
-  get listIsEmpty(): boolean {
-    return this.books.length <= 0;
-  }
-
-  create(): void {
-    this.router.navigate(['books/create']);
-  }
-
-  edit(id: number): void {
-    this.router.navigate(['books/edit', id]);
-  }
-
-  view(id: number): void {
-    this.router.navigate(['books/view', id]);
-  }
-
-  delete(): void {
-    this.crudService.delete('/books', this.removeItem).subscribe(result => {
-      this.postDelete();
-    });
-  }
-
-  confirmDelete(book: any): void {
-    this.removeItem = book;
-    this.modalService.open(ModalService.DELETE_MODAL_ID);
-  }
-
-  postDelete(): void {
-    this.listItems();
-    this.notificationService.deletedSucess();
+    this.listUtils.listItems();
   }
 
   createMasterDetail(): void {
     this.router.navigate(['books/create/master-detail']);
-  }
-
-  onChangePaginator(page: number): void {
-    this.currentPage = page - 1;
-    this.listItems();
-  }
-
-  onChangePageSize(pageSize: number): void {
-    this.pageSize = pageSize;
-    this.currentPage = 0;
-    this.listItems();
-  }
-
-  onSearch(searchText: string): void {
-    this.currentPage = 0;
-    this.currentSearch = searchText;
-    this.listItems();
-  }
-
-  generatePagination(): object {
-    return {
-      currentPage: this.currentPage,
-      pageSize: this.pageSize,
-      search: this.currentSearch
-    };
   }
 
 }
---
+++ library-front/src/app/shared/utils/list-utils.ts
@@ -0,0 +1,107 @@
+import { CrudService } from '../services/crud.service';
+import { ModalService } from '../services/modal.service';
+import { NotificationService } from '../services/notification.service';
+import { Router } from '@angular/router';
+
+export class ListUtils {
+
+  loading: boolean;
+  items: any[];
+  pageSize: number;
+  currentPage: number;
+  totalRecords: number;
+  totalPages: number;
+  removeItem: any;
+  currentSearch: string;
+
+  get listIsEmpty() {
+    return this.items.length < 0;
+  }
+
+  constructor(
+      private service: CrudService,
+      private router: Router,
+      private notificationService: NotificationService,
+      private modalService: ModalService,
+      private serviceUrl: string,
+      private routerUrl: string
+    ) {
+    this.initProperties();
+  }
+
+  initProperties() {
+    this.loading = false;
+    this.items = [];
+    this.pageSize = 10;
+    this.currentPage = 0;
+    this.totalRecords = 0;
+    this.totalPages = 0;
+    this.removeItem = null;
+    this.currentSearch = '';
+  }
+
+  listItems(customUrl?: string) {
+    this.loading = true;
+    this.service.getAll(customUrl ? customUrl : this.serviceUrl, this.generatePagination()).subscribe((res: any) => {
+      this.items = res.items;
+      this.pageSize = res.pageSize;
+      this.currentPage = res.currentPage;
+      this.totalRecords = res.totalRecords;
+      this.totalPages = res.totalPages;
+      this.loading = false;
+    }, _err => {
+      this.loading = false;
+    });
+  }
+
+  view(item: any) {
+    return this.router.navigate([this.routerUrl, 'view', item.id]);
+  }
+
+  edit(item: any) {
+    return this.router.navigate([this.routerUrl, 'edit', item.id]);
+  }
+
+  create() {
+    return this.router.navigate([this.routerUrl, 'create']);
+  }
+
+  generatePagination() {
+    return {
+      currentPage: this.currentPage,
+      pageSize: this.pageSize,
+      search: this.currentSearch
+    };
+  }
+
+  setRemove(item: any) {
+    this.removeItem = item;
+    this.modalService.open(ModalService.DELETE_MODAL_ID);
+  }
+
+  remove(customUrl?: string) {
+    this.service.delete(customUrl ? customUrl : this.serviceUrl, this.removeItem).subscribe(_res => {
+      this.listItems();
+      this.removeItem = null;
+    }, (err: any) => {
+      this.notificationService.errorMessage(err.error ? err.error.message : err.message, 'Delete Error');
+    });
+  }
+
+  onChangePaginator(page: number): void {
+    this.currentPage = page - 1;
+    this.listItems();
+  }
+
+  onChangePageSize(pageSize: number): void {
+    this.pageSize = pageSize;
+    this.currentPage = 0;
+    this.listItems();
+  }
+
+  onSearch(searchText: string): void {
+    this.currentPage = 0;
+    this.currentSearch = searchText;
+    this.listItems();
+  }
+}
